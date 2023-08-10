5b2dd8451d3145f516a537b679ba978aec168f2d
Yuriy Zagoryansky <my.ofice@gmail.com>
2023-08-10 03:09:01 +0300

    changed search components on the front page

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -12,12 +12,14 @@ class Search extends Component
 
     public function updatedSearch()
     {
-       // $this->query = strlen($this->search) >5 ? $this->search : null;
+        // $this->query = strlen($this->search) >5 ? $this->search : null;
+        $this->search = trim($this->search);
         $this->dispatch('searchResult', search: $this->search);
     }
 
     public function enterSearch()
     {
+        $this->search = trim($this->search);
         //$this->query = strlen($this->search) > 5 ? $this->search : null;
         $this->dispatch('searchResult', search: $this->search);
     }
